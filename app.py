a2662f97e4b82e18cb5c320b3a34f9257b082cf3
Apoorva Gupta <78782044+apoorvag712@users.noreply.github.com>
2023-08-31 20:34:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,7 @@ def main():
   num3 = st.number_input("Enter the third number:")
     
   if st.button("Find Largest"):
-    
     largest = find_largest(num1, num2, num3)
     st.write(f"The largest number is: {largest}")
 
-if __name__ == "__main__"
-  main()
+main()
